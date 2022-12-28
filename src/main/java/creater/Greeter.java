19f3686abedf7c82679e77d143e57ae330d1e6db
oresh247 <oresh247@gmail.com>
2022-12-28 12:18:47 +0300

    new version

---
+++ src/main/java/creater/Greeter.java
@@ -0,0 +1,7 @@
+package creater;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ src/main/java/creater/JSONCreater.java
@@ -0,0 +1,8 @@
+package creater;
+
+public class JSONCreater {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
